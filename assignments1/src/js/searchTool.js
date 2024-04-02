e48a3cfe7f77fd6979556b94f5c1cd07dd39c387
Samdanova <samdanova123@gmail.com>
2024-04-02 21:29:08 +0300

    added the correct logic

--- assignments1/src/js/searchTool.js
+++ assignments1/src/js/searchTool.js
@@ -6,31 +6,54 @@ const contactContainer = document.querySelector(".contact-container");
 
 function searchContact() {
   const inputValue = document.getElementById("input-contact").value.trim();
-  if (typeof inputValue === "string") {
-    const firstName = data.find(
-      (element) => element.name.first.toLowerCase() === inputValue.toLowerCase()
+  const phoneNumber = parseInt(inputValue.replace(/\D/g, ""));
+
+  if (!isNaN(phoneNumber)) {
+    // checked either phone is number
+    const contact = data.find(
+      (element) => normalNumber(element.phone) === normalNumber(inputValue)
     );
-    const lastName = data.find(
-      (element) => element.name.last.toLowerCase() === inputValue.toLowerCase()
-    );
-    firstName ? renderContact(firstName) : renderContact(lastName);
-  } else if (typeof inputValue === "number") {
-    const number = data.find(
-      (element) => element.name.last.toLowerCase() === inputValue.toLowerCase()
-    );
-    renderContact(number);
+    contact ? renderContact([contact]) : generateError();
   } else {
-    console.log("Wrong value, input name or number");
+    // if not, we find by name
+    const name = data.filter(
+      (element) =>
+        element.name.first.toLowerCase().includes(inputValue.toLowerCase()) ||
+        element.name.last.toLowerCase().includes(inputValue.toLowerCase())
+    );
+    name.length ? renderContact(name) : generateError();
   }
 }
 
-function renderContact(contact) {
-  const contactLine = document.createElement("div");
-  contactLine.innerHTML = `<p>${contact.name.first} ${contact.name.last}</p>
-  <p>Phone number: ${contact.phone}</p> 
-  <p>Email: ${contact.email}</p>`;
-  contactLine.classList.add("contact-item");
-  contactContainer.append(contactLine);
+function renderContact(contacts) {
+  //clean the data
+  document.getElementById("input-contact").value = "";
+  contactContainer.innerHTML = "";
+  //display contact
+  contacts.forEach((element) => {
+    const contactLine = document.createElement("div");
+    contactLine.innerHTML = `<p>${element.name.first} ${element.name.last}</p>
+    <p>Phone number: ${element.phone}</p> 
+    <p>Email: ${element.email}</p>`;
+    contactLine.classList.add("contact-item");
+    contactContainer.append(contactLine);
+  });
+}
+
+function generateError() {
+  //clean the data
+  document.getElementById("input-contact").value = "";
+  contactContainer.innerHTML = "";
+  //create a message for error
+  const errorDiv = document.createElement("div");
+  errorDiv.textContent =
+    "Try one more time please. You don't have a contact with such name or number";
+  errorDiv.className = "error-text";
+  contactContainer.append(errorDiv);
+}
+
+function normalNumber(phoneNumber) {
+  return phoneNumber.replace(/\D/g, "");
 }
 
 submitForm.addEventListener("submit", (e) => {
